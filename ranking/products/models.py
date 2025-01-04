3d8bfa1f42d9f6a888768d1601b3cdc47724c2b2
nicoaranc <nicolas.arancibia.aguirre@gmail.com>
2025-01-04 12:06:53 -0300

    Change Meta Options of Models & Migrations

--- ranking/products/models.py
+++ ranking/products/models.py
@@ -9,10 +9,31 @@ class Game(models.Model):
     score = models.IntegerField(verbose_name='Puntaje')
     image = models.CharField(max_length=300, verbose_name='Imagen')
 
+    class Meta:
+        verbose_name = 'Juego'
+        verbose_name_plural = 'Juegos'
+        ordering = ['score']
+
+    def __str__(self):
+        return self.name
+
 class Platform(models.Model):
     name = models.CharField(max_length=4, verbose_name='Nombre', primary_key=True)
+
+    class Meta:
+        verbose_name = 'Plataforma'
+        verbose_name_plural = 'Plataformas'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 class Image(models.Model):
     route = models.CharField(max_length=300, verbose_name='Ruta')
     game = models.IntegerField(verbose_name='Game Id', primary_key=True)
+
+    class Meta:
+        verbose_name = 'Imagen'
+        verbose_name_plural = 'Imágenes'
+        ordering = ['game']
 
