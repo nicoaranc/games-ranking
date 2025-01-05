4f36d440f8366309053808d449052b8b142c2bc0
nicoaranc <nicolas.arancibia.aguirre@gmail.com>
2025-01-05 01:52:02 -0300

    Add Home View (to fix)

--- ranking/vue-django/vue-ranking/src/main.js
+++ ranking/vue-django/vue-ranking/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+createApp(App).use(axios).mount('#app')
---
+++ ranking/vue-django/vue-ranking/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="home">
+        <h1>Mi Ranking de juegos</h1>
+    </div>
+    <div>
+        <div>
+            <h2>Mejores juegos</h2>
+        </div>
+        <div 
+        v-for="game in Games"
+        v-bind:key="game.id"
+        >
+        <h3>{{ game.name }}</h3>
+        <p>{{ game.score }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name:'Home-page',
+    data() {
+        return {
+            Games: []
+        }
+    },
+    components: {
+    },
+    mounted() {
+        this.getBestGames()
+    },
+    methods: {
+        getBestGames() {
+            axios
+                .get('/api/games/')
+                .then(response => {
+                    this.Games = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }
+}
+</script>
