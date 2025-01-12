b340d4752b8c5060ee3e7b7d196672d1b77312f9
nicoaranc <nicolas.arancibia.aguirre@gmail.com>
2025-01-12 16:38:11 -0300

    Center boxes text from Platform view

--- ranking/vue-django/vue-ranking/src/views/Platform.vue
+++ ranking/vue-django/vue-ranking/src/views/Platform.vue
@@ -23,11 +23,11 @@
                     <img :src="game.get_thumbnail">
                 </figure>
 
-                <h3 class="is-size-4">{{ game.name }}</h3>
-                <h4 class="is-size-6">{{ game.platform }}</h4>
-                <p class="is-size-6 has-text-grey">{{ game.score }}/100</p>
+                <h3 class="is-size-4 has-text-centered">{{ game.name }}</h3>
+                <h4 class="is-size-6 has-text-centered">{{ game.platform }}</h4>
+                <p class="is-size-6 has-text-grey has-text-centered">{{ game.score }}/100</p>
 
-                <router-link v-bind:to="game.get_absolute_url" class="button is-dark mt-4"> Ver juego </router-link>
+                <router-link v-bind:to="game.get_absolute_url" class="button is-dark mt-4 has-text-centered"> Ver juego </router-link>
             </div>
 
         </div>
